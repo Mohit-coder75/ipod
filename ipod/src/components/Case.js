64ca812ee96d6dd160c8145bdeec2470fc6b4590
Mohit <pa.mohit47@gmail.com>
2025-05-01 00:29:45 +0530

    Development Complted

---
+++ ipod/src/components/Case.js
@@ -0,0 +1,23 @@
+import { Component } from "react";
+import Wheel from './Wheel.js'
+import Display from './Display.js'
+import '../css/Case.css';
+
+
+class Case extends Component {
+
+    render() {
+        const {active,updateActiveMenu, currentMenu, changeMenuBackward,changeMenuForward, menuItems, musicItems,togglePlayPause, songItems,playing, songIndex,theme, audio, songUrl, songImgUrl, seekSongForward, seekSongReverse, wheelColor ,wallpaper, wallpaperItems, noty, setNoty, notifyText} = this.props;
+
+        return (
+            <div className="case-container" >
+                <div  style={{backgroundColor: 'rgb(137, 97, 97)'}}  className="case">
+                <Display songIndex={songIndex} playing={playing} active={active} musicItems={musicItems} menuItems={menuItems} currentMenu={currentMenu} songItems={songItems} audio={audio} songUrl={songUrl} songImgUrl={songImgUrl} wallpaper={wallpaper} wallpaperItems={wallpaperItems} noty={noty} setNoty={setNoty} notifyText ={notifyText}/>
+                <Wheel theme={theme} active={active} menuItems={menuItems} currentMenu={currentMenu} changeMenuForward={changeMenuForward} changeMenuBackward={changeMenuBackward} updateActiveMenu={updateActiveMenu} togglePlayPause={togglePlayPause} seekSongForward={seekSongForward} seekSongReverse={seekSongReverse} wheelColor={wheelColor}/>
+                </div>
+                
+            </div>
+        );
+    }
+}
+export default Case;
